bc313d6bd9ca0e962cb4cb8d6ec2c8526698afd5
Strashimir Yosifov <syosifov@gmail.com>
2022-03-29 11:59:50 +0300

    Initial project setup.

---
+++ src/main/java/net/yosifov/accounting/accj/accj2gateway/Accj2GatewayApplication.java
@@ -0,0 +1,13 @@
+package net.yosifov.accounting.accj.accj2gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accj2GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accj2GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yosifov/accounting/accj/accj2gateway/Accj2GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yosifov.accounting.accj.accj2gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accj2GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

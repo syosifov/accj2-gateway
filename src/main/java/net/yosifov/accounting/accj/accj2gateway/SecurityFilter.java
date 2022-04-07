c81677e27a24eefbaf7eee81aa1a659ef1213460
Strashimir Yosifov <syosifov@gmail.com>
2022-04-07 20:10:45 +0300

    JWT verified

--- src/main/java/net/yosifov/accounting/accj/accj2gateway/SecurityFilter.java
+++ src/main/java/net/yosifov/accounting/accj/accj2gateway/SecurityFilter.java
@@ -1,5 +1,9 @@
 package net.yosifov.accounting.accj.accj2gateway;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +19,11 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.crypto.SecretKey;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 // https://www.programcreek.com/java-api-examples/?api=org.springframework.cloud.gateway.filter.GlobalFilter
 @Component
@@ -35,11 +43,20 @@ public class SecurityFilter implements GlobalFilter {
         logger.info("Path of the request received -> {}",
                 exchange.getRequest().getPath());
         ServerHttpRequest request = exchange.getRequest();
-        System.out.println(request);
         RequestPath path = request.getPath();
-        System.out.println(path);
-        if (path.toString().equals("/get") && false) {
+        String sPath = path.toString();
+
+        if (sPath.equals("/get") && false) {
             throw new RuntimeException("Bla bla bla");
+        }
+        if(sPath.equals("/api/v1/auth/login")) {
+            return chain.filter(exchange);
+        }
+        if(sPath.equals("/api/v1/auth/refresh")) {
+            return chain.filter(exchange);
+        }
+        if(sPath.equals("/api/v1/auth/testRequest")) {
+            return chain.filter(exchange);
         }
         if(isBad(exchange)) {
             exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
@@ -55,7 +72,42 @@ public class SecurityFilter implements GlobalFilter {
         RequestPath path = request.getPath();
         HttpHeaders httpHeaders = request.getHeaders();
         String token = getToken(httpHeaders);
+        if(null == token || token.trim().isEmpty()) {
+            return true;
+        }
 
+        try {
+            Jws<Claims> claimsJws = Jwts
+                    .parserBuilder()
+                    .setSigningKey(appSecretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+
+            String username = claims.getSubject();
+            Long id = claims.get("id", Long.class);
+
+            Map<String,Object> principalDataMap = new HashMap<>();
+            principalDataMap.put("username", username);
+            principalDataMap.put("id", id);
+
+            var authorities = (List<Map<String, Object>>) claims.get("authorities");
+
+//            Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+//                    .map(m -> new SimpleGrantedAuthority((m.get("authority")).toString()))
+//                    .collect(Collectors.toSet());
+//
+//            Authentication authentication = new UsernamePasswordAuthenticationToken(
+//                    principalDataMap,
+//                    null,
+//                    simpleGrantedAuthorities
+//            );
+//
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        } catch (JwtException e) {
+            throw new IllegalStateException(String.format("Token cannot be trusted: %s", token));
+        }
 
 
         return false;
